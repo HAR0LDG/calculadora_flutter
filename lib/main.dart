a33ea801143e2c38006d3e3315f4aff19f818fd6
HAR0LDG <he.gonzalez100@gmail.com>
2024-10-10 13:42:55 -0500

    Creacion del issue historial

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main(List<String> args) {
   runApp(const CalculadoraApp());
@@ -14,6 +15,29 @@ class CalculadoraApp extends StatefulWidget {
 class _CalculadoraAppState extends State<CalculadoraApp> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Calculadora",
+      home: Scaffold(
+        backgroundColor: const Color(0xff283637),
+        body: Container(
+          padding: const EdgeInsets.all(12),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(right: 12.0),
+                child: Text(
+                  "Historial",
+                  style: GoogleFonts.rubik(
+                    textStyle: const TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
